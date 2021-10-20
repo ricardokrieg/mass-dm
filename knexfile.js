c2a6cd0b2b3320a4e7d4a0aa3d30366449ab063a
Ricardo Franco <ricardo.krieg@gmail.com>
2021-10-20 19:09:09 -0300

    update production database env vars

--- knexfile.js
+++ knexfile.js
@@ -19,11 +19,6 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_NAME,
-      host:     process.env.DATABASE_HOST,
-      user:     process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-    },
+    connection: process.env.DATABASE_URL,
   },
 };
